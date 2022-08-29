e27ef4bee9e88b422ceaaa81fd7ec86dceb7a8f1
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2022-08-29 18:50:52 +0200

    ADD: random quote get function

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
-
+const PORT = process.env.PORT || 4002;
 app.use(express.static('public'));
 
 app.listen(PORT);
+
+app.use('/api', router);
+
+//GET a random Quote
+router.get('/quotes/random', (req, res, next) => {
+    const quote = getRandomElement(quotes);
+    res.send({quote: quote});
+})
+
